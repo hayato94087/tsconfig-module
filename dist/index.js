e15eec61c7b261913ddf13d17cad76e56337d1e1
hayato94087 <hayato94087@gmail.com>
2024-03-19 23:29:12 +0900

    feat: モジュールの管理をES6に変更

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,3 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const settings_json_1 = __importDefault(require("./settings.json"));
-console.log(settings_json_1.default);
+import settings from "./settings.json";
+console.log(settings);
 //# sourceMappingURL=index.js.map
